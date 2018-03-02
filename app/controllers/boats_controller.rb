c1989b9544809bbaf072ba1d01f7dd026ff7e0e2
Zack Enders <enders.zack@gmail.com>
2018-03-02 10:50:21 -0500

    Added boat pages and setup boat display

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,22 +1,27 @@
 class BoatsController < ApplicationController
 
 	def index 
-
+		@boats = Boat.all
 	end
 
 
 	def new
-
+		@boat = Boat.new
 	end
 
 
 	def create
-
+		@boat = Boat.new(boat_params)
+		# if @boat.save
+		# 	redirect_to 
+		# else
+		# 	render
+		# end
 	end
 
 
 	def show
-
+		@boat = Boat.find(params[:id])
 	end
 
 
@@ -36,7 +41,10 @@ class BoatsController < ApplicationController
 
 	private 
 
-	def
+	def boat_params
+		
+		params.require(:boat).permit(:boat_name, :capacity, :location)
+	end
 
 
 end
