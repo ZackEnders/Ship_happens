4638fe6e41dbc2c36a203af5101632612035e955
Brandon P <brandonpegula@gmail.com>
2018-03-02 13:12:25 -0500

    Navbar updates, problem with Profile resolved

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,45 @@
 class ClientsController < ApplicationController
+
+	before_action :authenticate_client!
+	def assign
+
+	end
+
+
+	def index
+	
+	@clients = Client.all
+
+	end
+
+
+	def new
+ 
+	end
+
+
+	def create 
+		
+	end
+
+
+	def show
+		render :layout => false
+	@client = Client.find(current_client.id)
+	end
+
+
+	def edit
+
+	end
+
+
+	def update
+
+	end
+
+
+	def destroy
+		
+	end
 end
