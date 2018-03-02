10f1b5447854242fbeff76abb00aa6e9953fde39
JordynK91 <JordynK91@gmail.com>
2018-03-02 16:38:13 -0500

    form for setting rate and name on user page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :clients
   root to: 'home#index'
   
-  resources :clients, only: [:show]
+  resources :clients, only: [:show, :update]
 
   resources :boats
   resources :jobs
