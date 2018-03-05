cf507e1a18e1f091c17a6a677d1d89325a7c112c
Zack Enders <enders.zack@gmail.com>
2018-03-05 18:06:51 -0500

    Fixed homepage styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,13 @@ class ApplicationController < ActionController::Base
 
   protected
 
+		def after_sign_up_path_for
+      		jobs_path
+    	end
+
+    	def after_update_path_for
+     		jobss_path
+    	end
         def configure_permitted_parameters
             devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name, :password, :password_confirmation]) 
             devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :password_confirmation, :current_password, :rate]) 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
 	def index 
 		@clients = Client.all
-		render :layout => false
+		render :layout => false 
 	end
 
 end
