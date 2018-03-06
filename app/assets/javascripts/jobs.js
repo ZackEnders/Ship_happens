71c0f939e83374ae37eed18830352423cc3f7d81
JordynK91 <JordynK91@gmail.com>
2018-03-06 12:14:18 -0500

    comments

--- app/assets/javascripts/jobs.js
+++ app/assets/javascripts/jobs.js
@@ -1,6 +1,15 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
+
+
+//this script powers a calculator to determine the cost of the job.
+// the user's rate is determined by their settings. user enters the number of containers.
+//	cost of job is determined by multiplying rate * containers
+// the cost of job and the number of containers are then popped into the below job creation form
+
+
+
 window.onload = function(){
 
 var cost_button = document.getElementById('cost_button')
