0b6c0a140cf3e5b79bad0c11dd75c591cc0aa34c
JordynK91 <JordynK91@gmail.com>
2018-03-06 11:30:09 -0500

    working on javascript

--- app/assets/javascripts/jobs.js
+++ app/assets/javascripts/jobs.js
@@ -1,12 +1,16 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
+window.onload = function(){
+
+var containers = document.getElementById('containers').value
+var rate = document.getElementById('rate').value
+var cost_button = document.getElementById('cost_button')
+cost_button.addEventListener('click', calcCost)
 
 function calcCost(){
-	var amountValue = document.getElementById('job_amount_of_c').value
-	var jobCost = document.getElementById('job_cost')
-	jobCost.innerHTML = amountValue
-}
+	var cost = document.getElementById('cost')
+	cost.innerHTML = containers * rate
 
-var button = document.getElementById('cost_button')
-button.addEventListener('click', calcCost)
+}
+}
