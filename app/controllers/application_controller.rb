d70859770da6cd4ca6bab7a81459374e4020a12d
Zack Enders <enders.zack@gmail.com>
2018-03-05 20:56:13 -0500

    Changed the redirect after login, logout, signup, and update account.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,15 +5,31 @@ class ApplicationController < ActionController::Base
   #Devise 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  
+
+
+
+  private 
+  		def after_sign_up_path_for(resource)
+      		"/clients/#{current_client.id}"
+    	end
+
+    	def after_update_path_for(resource)
+     		"/clients/#{current_client.id}"
+    	end
+
+    	def after_sign_in_path_for(resource)
+      		"/clients/#{current_client.id}"
+    	end
+
+    	def after_sign_out_path_for(resource)
+      		"/clients/sign_in"
+    	end
+
+
   protected
 
-		def after_sign_up_path_for
-      		jobs_path
-    	end
-
-    	def after_update_path_for
-     		jobss_path
-    	end
+		
         def configure_permitted_parameters
             devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name, :password, :password_confirmation]) 
             devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :password_confirmation, :current_password, :rate]) 
