cff764e172f27907a22e831d31cb7c913e4a8394
JordynK91 <JordynK91@gmail.com>
2018-03-06 11:43:08 -0500

    rate form works

--- app/assets/javascripts/jobs.js
+++ app/assets/javascripts/jobs.js
@@ -3,14 +3,18 @@
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 window.onload = function(){
 
-var containers = document.getElementById('containers').value
-var rate = document.getElementById('rate').value
 var cost_button = document.getElementById('cost_button')
 cost_button.addEventListener('click', calcCost)
 
 function calcCost(){
+	var containers = document.getElementById('containers').value
+	var rate = document.getElementById('rate').value
 	var cost = document.getElementById('cost')
-	cost.innerHTML = containers * rate
+	var job_cost = document.getElementById('job_cost')
+	total = containers * rate
+	cost.innerHTML = total
+	job_cost.value = total
+	job_amount_of_c.value = containers
 
 }
 }
